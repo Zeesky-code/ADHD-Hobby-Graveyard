455f439a22e6791bbbd9fb8e4e0160c27bb946c0
Zainab Lawal <zainablaw1012@gmail.com>
2021-10-11 01:31:18 +0300

    Added a verbose name to the model

--- hobbyGraveyard/hobbies/models.py
+++ hobbyGraveyard/hobbies/models.py
@@ -6,6 +6,8 @@ class Hobby(models.Model):
     title = models.CharField(max_length=200)
     stillInterested = models.BooleanField(default=False)
     created =  models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name_plural = "Hobbies"
 
     def __str__(self):
         return self.title
